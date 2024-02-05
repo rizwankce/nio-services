6ce87f41d181b991850e593b55131bdcf7380f64
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-02-05 11:42:05 +0530

    Refactor PingChannelHandler to its own file

---
+++ PingService/Sources/PingChannelHandler.swift
@@ -0,0 +1,58 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rizwan on 05/02/24.
+//
+
+import Foundation
+import NIOCore
+import NIOPosix
+import NIOHTTP1
+
+private final class PingChannelHandler: ChannelInboundHandler {
+    public typealias InboundIn = HTTPServerRequestPart
+    public typealias OutboundOut = HTTPServerResponsePart
+
+    public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        let requestPart = self.unwrapInboundIn(data)
+        switch requestPart {
+            case .head(let request):
+                if request.uri.unicodeScalars.starts(with: "/ping".unicodeScalars) {
+                    let responseTime = Int64.random(in: 20 ... 5000)
+                    context.eventLoop.scheduleTask(in: .milliseconds(responseTime)) {
+                        let response = HTTPServerResponsePart.head(HTTPResponseHead(version: .http1_1, status: .ok))
+                        let body = HTTPServerResponsePart.body(.byteBuffer(ByteBuffer(string: "OK")))
+                        let end = HTTPServerResponsePart.end(nil)
+                        context.write(self.wrapOutboundOut(response), promise: nil)
+                        context.write(self.wrapOutboundOut(body), promise: nil)
+                        context.writeAndFlush(self.wrapOutboundOut(end)).whenComplete { _ in
+                            context.close(promise: nil)
+                        }
+                    }
+                }
+                else {
+                    let notFoundResponse = HTTPServerResponsePart.head(HTTPResponseHead(version: .http1_1, status: .notFound))
+                    let end = HTTPServerResponsePart.end(nil)
+                    context.write(self.wrapOutboundOut(notFoundResponse), promise: nil)
+                    context.writeAndFlush(self.wrapOutboundOut(end)).whenComplete { _ in
+                        context.close(promise: nil)
+                    }
+                }
+
+            case .body:
+                break
+            case .end:
+                break
+        }
+    }
+
+    public func channelReadComplete(context: ChannelHandlerContext) {
+        context.flush()
+    }
+
+    public func errorCaught(context: ChannelHandlerContext, error: Error) {
+        print("error: ", error)
+        context.close(promise: nil)
+    }
+}
