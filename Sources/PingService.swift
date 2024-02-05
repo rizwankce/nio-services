1fc69701867b57a251730291246834d39dc25ee7
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-02-05 11:19:59 +0530

    Add random ping response between 20 to 5000 ms

--- Sources/PingService.swift
+++ Sources/PingService.swift
@@ -59,13 +59,16 @@ private final class PingChannelHandler: ChannelInboundHandler {
         switch requestPart {
             case .head(let request):
                 if request.uri.unicodeScalars.starts(with: "/ping".unicodeScalars) {
-                    let response = HTTPServerResponsePart.head(HTTPResponseHead(version: .http1_1, status: .ok))
-                    let body = HTTPServerResponsePart.body(.byteBuffer(ByteBuffer(string: "OK")))
-                    let end = HTTPServerResponsePart.end(nil)
-                    context.write(self.wrapOutboundOut(response), promise: nil)
-                    context.write(self.wrapOutboundOut(body), promise: nil)
-                    context.writeAndFlush(self.wrapOutboundOut(end)).whenComplete { _ in
-                        context.close(promise: nil)
+                    let responseTime = Int64.random(in: 20 ... 5000)
+                    context.eventLoop.scheduleTask(in: .milliseconds(responseTime)) {
+                        let response = HTTPServerResponsePart.head(HTTPResponseHead(version: .http1_1, status: .ok))
+                        let body = HTTPServerResponsePart.body(.byteBuffer(ByteBuffer(string: "OK")))
+                        let end = HTTPServerResponsePart.end(nil)
+                        context.write(self.wrapOutboundOut(response), promise: nil)
+                        context.write(self.wrapOutboundOut(body), promise: nil)
+                        context.writeAndFlush(self.wrapOutboundOut(end)).whenComplete { _ in
+                            context.close(promise: nil)
+                        }
                     }
                 }
                 else {
