92bcfea8ce2675277b9ec6d9af2bedccf5d5eb53
Rizwan Mohamed Ibrahim <rizwankce@users.noreply.github.com>
2024-02-06 12:03:43 +0530

    Refactor data processor

--- PingService/Sources/StatsDataProcessor.swift
+++ PingService/Sources/StatsDataProcessor.swift
@@ -1,6 +1,6 @@
 //
 //  JSONExporter.swift
-//  
+//
 //
 //  Created by Rizwan on 06/02/24.
 //
@@ -10,13 +10,65 @@ import NIOFileSystem
 import NIOCore
 
 public class StatsDataProcessor {
-    let filePath: String
+    private let filePath: String
+    private let fileSystem: FileSystem = FileSystem.shared
 
     init(filePath: String) {
         self.filePath = filePath
     }
 
-    func getDirectoryPath() -> String {
+    public func export(_ statsResponseModel: StatsResponseModel) async throws {
+        try await purgeOldDataIfNeeded()
+
+        let directoryPath = getDirectoryPath()
+        let timestamp = getTimestamp()
+
+        let path = filePath + directoryPath + "/\(timestamp).json"
+        print("Stats to be saved :\(statsResponseModel) at path : \(path)")
+
+        do {
+            if await !isDirectoryExists(atPath: filePath + directoryPath) {
+                try await FileSystem.shared.createDirectory(at: FilePath(filePath + directoryPath), withIntermediateDirectories: true)
+            }
+            try await FileSystem.shared.withFileHandle(
+                forWritingAt:  FilePath(path),
+                options: .newFile(replaceExisting: true)
+            ) { file in
+                var buffer: ByteBuffer =  ByteBufferAllocator().buffer(capacity: 1024)
+                let encoder = JSONEncoder()
+                encoder.outputFormatting = .prettyPrinted
+                try JSONEncoder().encode(statsResponseModel, into: &buffer)
+                try await file.write(contentsOf: buffer.readableBytesView, toAbsoluteOffset: 0)
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
+
+    public func purgeOldDataIfNeeded() async throws {
+        let twoDaysAgo = Calendar.current.date(byAdding: .day, value: -2, to: Date())!
+        try await removeDirectoriesOlderThan(twoDaysAgo)
+    }
+
+    private func removeDirectoriesOlderThan(_ date: Date) async throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        // Get the day
+        dateFormatter.dateFormat = "dd"
+        let day = Int(dateFormatter.string(from: date))!
+        print("Trying to purge old data :\(date) \(day)")
+        try await FileSystem.shared.withDirectoryHandle(atPath: FilePath(filePath)) { directory in
+            for try await entry in directory.listContents() {
+                if entry.type == .directory && day >= Int(entry.name.string)! {
+                    print("removing old directory at path \(entry.path)")
+                    try await FileSystem.shared.removeItem(at: entry.path)
+                }
+            }
+        }
+    }
+
+    private func getDirectoryPath() -> String {
         let now = Date()
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = TimeZone.current
@@ -35,7 +87,7 @@ public class StatsDataProcessor {
         return directoryPath
     }
 
-    func getTimestamp() -> String {
+    private func getTimestamp() -> String {
         let now = Date()
         let dateFormatter = ISO8601DateFormatter()
         dateFormatter.formatOptions.insert(.withFractionalSeconds)
@@ -44,7 +96,7 @@ public class StatsDataProcessor {
         return timestamp
     }
 
-    func isDirectoryExsists(atPath path: String) async -> Bool {
+    private func isDirectoryExists(atPath path: String) async -> Bool {
         do {
             return try await FileSystem.shared.withDirectoryHandle(atPath: FilePath(path)) { directory -> Bool in
                 _ = try await directory.info()
@@ -53,57 +105,6 @@ public class StatsDataProcessor {
         }
         catch {
             return false
-        }
-    }
-
-    func export(_ statsResponseModel: StatsResponseModel) {
-        purgeOldDataIfNeeded()
-
-        let directoryPath = getDirectoryPath()
-        let timestamp = getTimestamp()
-        print(directoryPath, timestamp)
-        let path = filePath + directoryPath + "/\(timestamp).json"
-        print("Stats to be saved :\(statsResponseModel) at path : \(path)")
-
-        Task {
-            do {
-                if await !isDirectoryExsists(atPath: filePath + directoryPath) {
-                    try await FileSystem.shared.createDirectory(at: FilePath(filePath + directoryPath), withIntermediateDirectories: true)
-                }
-                try await FileSystem.shared.withFileHandle(
-                    forWritingAt:  FilePath(path),
-                    options: .newFile(replaceExisting: true)
-                ) { file in
-                    var buffer: ByteBuffer =  ByteBufferAllocator().buffer(capacity: 1024)
-                    let encoder = JSONEncoder()
-                    encoder.outputFormatting = .prettyPrinted
-                    try JSONEncoder().encode(statsResponseModel, into: &buffer)
-                    try await file.write(contentsOf: buffer.readableBytesView, toAbsoluteOffset: 0)
-                }
-            }
-            catch {
-                print(error)
-            }
-        }
-    }
-
-    func purgeOldDataIfNeeded() {
-        let twoDaysAgo = Calendar.current.date(byAdding: .day, value: -2, to: Date())!
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = TimeZone.current
-        // Get the day
-        dateFormatter.dateFormat = "dd"
-        let day = Int(dateFormatter.string(from: twoDaysAgo))!
-        print("Trying to purge old data :\(twoDaysAgo) \(day)")
-        Task {
-            try await FileSystem.shared.withDirectoryHandle(atPath: FilePath(filePath)) { directory in
-                for try await entry in directory.listContents() {
-                    if entry.type == .directory && day >= Int(entry.name.string)! {
-                        print("removing old directory at path \(entry.path)")
-                        try await FileSystem.shared.removeItem(at: entry.path)
-                    }
-                }
-            }
         }
     }
 }
